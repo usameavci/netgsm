09d397446e11668dbbca6b074f46247bb5656cea
Phpuzem <phpuzem@gmail.com>
2015-05-28 10:41:00 +0300

    Updated

---
+++ src/Phpuzem/Netgsm/SmsServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Phpuzem\Netgsm;
+use Phpuzem\Netgsm\SmsManagement;
+use Illuminate\Support\ServiceProvider;
+
+class SmsServiceProvider extends ServiceProvider {
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/sms.php' => config_path('sms/sms.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['sms'] = $this->app->share(function($app)
+        {
+            return new SmsManagement;
+        });
+        $this->app->alias('sms', 'Phpuzem\Netgsm\SmsManagement');
+    }
+
+}
